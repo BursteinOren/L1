ebe24f860203e518b3c1d8e525536c6be3dd28ba
Burstein Oren <oren.burstein@gamil.com>
2016-04-02 16:45:24 +0300

    FileUpload

---
+++ L1/HomePage.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="L1.HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged" ForeColor="YellowGreen" BackColor="BlueViolet"></asp:Calendar>
+        <br />
+        <br />
+        <asp:Label ID="lblCalendar1" runat="server" Text=""></asp:Label>
+        <asp:Button ID="btnOK" runat="server" Text="OK" />
+        <br />
+        <br />
+        <asp:FileUpload ID="FileUpload1" runat="server"   />
+        <br />
+        <asp:Button ID="btnSave" runat="server" Text="Upload" OnClick="btnSave_Click"  />
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ L1/HomePage.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.IO;
+
+
+namespace L1
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Calendar1.BackColor = Color.BlueViolet;
+            }
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            lblCalendar1.Text = Calendar1.SelectedDate.ToShortDateString();
+            if (Calendar1.BackColor == Color.Yellow)
+                Calendar1.BackColor = Color.Green;
+            else Calendar1.BackColor = Color.Yellow;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            Guid guid = Guid.NewGuid();
+            //FileUpload1.SaveAs(MapPath("MyFiles") + "/" + guid + "_" + FileUpload1.FileName);   
+            FileUpload1.SaveAs(MapPath("MyFiles") + "/" + guid + Path.GetExtension(FileUpload1.FileName));
+        }
+    }
+}
